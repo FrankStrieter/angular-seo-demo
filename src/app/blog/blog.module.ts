5b7246b7bc4841ff1b79eb6e216944907eee5495
Frank Strieter <fs@co-it.eu>
2018-05-22 15:32:30 +0200

    adds article and dashboard component

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [DashboardComponent, ArticleComponent]
 })
 export class BlogModule { }
