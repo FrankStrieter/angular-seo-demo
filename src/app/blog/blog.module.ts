4d202cfb6af0a17108f46261ae43cb6482c5bad0
Frank Strieter <fs@co-it.eu>
2018-05-22 15:46:15 +0200

    moves article-preview to blog-module

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ArticleComponent } from './article/article.component';
+import { ArticlePreviewComponent } from './article-preview/article-preview.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [DashboardComponent, ArticleComponent]
+  declarations: [DashboardComponent, ArticleComponent, ArticlePreviewComponent]
 })
 export class BlogModule { }
