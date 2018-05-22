3c4e613b7f200505a72706a0fd6dbdf84d6f2e9d
Frank Strieter <fs@co-it.eu>
2018-05-22 15:33:17 +0200

    adds article-preview component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ArticlePreviewComponent } from './article-preview/article-preview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticlePreviewComponent
   ],
   imports: [
     BrowserModule
