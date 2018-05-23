b75e2d41c4e4005a379df37b3b0d9834f8dbf1e2
Frank Strieter <fs@co-it.eu>
2018-05-23 09:28:16 +0200

    adds moderation module

---
+++ src/app/moderation/moderation.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ModerationRoutingModule } from './moderation-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ModerationRoutingModule
+  ],
+  declarations: []
+})
+export class ModerationModule { }
